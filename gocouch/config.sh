5754fc4b071bd83bf1e23b7a7ad1d63c37bafa8b
Dragos STOICA <dragos_constantin_stoica@yahoo.com>
2016-02-23 15:49:17 +0200

    Update config.sh

--- gocouch/config.sh
+++ gocouch/config.sh
@@ -1,1 +1,21 @@
 
+#!/bin/bash
+#
+# Setup for GOPATH variable
+# Import all frameworks and packages from github
+# build and run the gocouch database
+#
+# Author - Dragos STOICA
+# Date: 23.02.2016
+#
+
+export GOPATH=`pwd`
+
+git clone ./src/github.com/boltdb/bolt
+git clone ./src/github.com/
+git clone ./src/github.com/
+git clone ./src/github.com/
+git clone ./src/github.com/
+git clone ./src/github.com/
+git clone ./src/github.com/
+git clone ./src/github.com/
